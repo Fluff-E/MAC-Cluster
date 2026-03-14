e989b546859fa108eea2a73a7a524ca58044b211
Eric Yates <k.ericyates@gmail.com>
2026-03-14 09:25:04 -0400

    compiled hardware

---
+++ hps_fpga_Project/soc_system/synthesis/submodules/mac_2x2.v
@@ -0,0 +1,55 @@
+module mac_2x2 (
+	input  wire         clk,
+	input  wire         reset,
+
+	// Handshake
+	input  wire         start,
+	output reg          done,
+
+	// Matrix A row ai
+	input  wire signed [31:0] a0,
+	input  wire signed [31:0] a1,
+
+	// Matrix B column bi
+	input  wire signed [31:0] b0,
+	input  wire signed [31:0] b1,
+
+	
+	// Output Matrix O entry oi
+	output reg signed [63:0] result
+);
+
+/*
+	[ a0 a1 ]   [ b0 ]   [ o0 ]
+	[       ] * [ b1 ] = [    ]
+*/
+
+	reg busy;
+
+	always @(posedge clk or posedge reset) begin
+		if (reset) begin
+			result <= 0;
+			done   <= 0;
+			busy   <= 0;
+		end
+		else begin
+			done <= 0;
+
+			// Start computation
+			if (start && !busy) begin
+				busy <= 1;
+			end
+
+			// One-cycle dot product
+			else if (busy) begin
+				result <=
+					(a0 * b0) +
+					(a1 * b1);
+
+				done <= 1;
+				busy <= 0;
+			end
+		end
+	end
+
+endmodule
